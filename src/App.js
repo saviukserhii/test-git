97d060c90efb7084993e645d7964bdf8580e866c
“Serhii_Saviuk” <s.saviuk@apiko.com>
2022-07-04 13:07:28 +0300

    to prod

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>third branch</div>
+        <div>second commit</div>
         <a
           className="App-link"
           href="https://reactjs.org"
