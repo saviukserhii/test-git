b30aabfcc470d35b06ca41ca4d19b8da1c94ed79
“Serhii_Saviuk” <s.saviuk@apiko.com>
2022-07-04 13:07:28 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>third branch</div>
+        <div>second commit</div>
         <a
           className="App-link"
           href="https://reactjs.org"
